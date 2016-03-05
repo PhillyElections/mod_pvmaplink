2c5c1cee129c0f6e30f2eea30e30213a2fe0ff53
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-05 16:56:28 -0500

    first version of form horseshoe'd in

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,4 +1,7 @@
-<?php defined('_JEXEC') or die('Restricted access'); // no direct access ?>
+<?php defined('_JEXEC') or die('Restricted access');
+
+$document->addCustomTag('<script src="/modules/mod_maplink/js/maplink.js" async defer></script>');
+?>
 <?php echo JText::_('RANDOM USERS'); ?>
 <ul>
     <?php foreach ($items as $item) {?>
@@ -8,3 +11,11 @@
     <?php }
 ?>
 </ul>
+<form action="<?=JRoute::_('index.php?option=com_voterapp');?>" method="post" id="josForm" name="josForm" class="form-validate">
+
+        <input type="text" id="address1" name="address1" size="60%" value="<?=$address1;?>" class="inputbox required" maxlength="60" placeholder="<?=JText::_('STREET PLACEHOLDER');?>" />
+
+        <button class="button validate" type="submit"><?=JText::_('REGISTER');?></button>
+
+<?=JHTML::_('form.token');?>
+</form>
