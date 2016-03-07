8d05888ac6d09eae5e4762e1baf710b301ebe713
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-07 16:06:19 -0500

    inline css

--- mod_maplink.php
+++ mod_maplink.php
@@ -7,6 +7,11 @@ $language->load('mod_maplink', JPATH_ADMIN);
 $document = &JFactory::getDocument();
 $document->addCustomTag('<script src="/modules/mod_maplink/js/maplink.js" async defer></script>');
 ?>
+<style>
+.pac-container, .pac-item {
+    width: inherit !important;
+}
+</style>
 <form action="<?=JRoute::_('index.php?option=com_voterapp');?>" method="post" id="josForm" name="josForm" class="form-validate">
     <input type="text" id="address1" name="address1" width="23em" value="" class="inputbox" placeholder="<?=JText::_('ADDRESS PLACEHOLDER');?>" />
 </form>
