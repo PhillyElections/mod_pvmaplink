18b8edc79347dbdd7ac57fdec896c28d749ecf5b
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-07 15:19:48 -0500

    tweaking width

--- mod_maplink.php
+++ mod_maplink.php
@@ -8,5 +8,5 @@ $document = &JFactory::getDocument();
 $document->addCustomTag('<script src="/modules/mod_maplink/js/maplink.js" async defer></script>');
 ?>
 <form action="<?=JRoute::_('index.php?option=com_voterapp');?>" method="post" id="josForm" name="josForm" class="form-validate">
-    <input type="text" id="address1" name="address1" width="20em" value="<?=$address1;?>" class="inputbox" placeholder="<?=JText::_('ADDRESS PLACEHOLDER');?>" />
+    <input type="text" id="address1" name="address1" width="23em" value="<?=$address1;?>" class="inputbox" placeholder="<?=JText::_('ADDRESS PLACEHOLDER');?>" />
 </form>
