fa1c3649343766f1cf2184a078c026f1de6ee0c6
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-07 15:15:01 -0500

    fixing translation string

--- mod_maplink.php
+++ mod_maplink.php
@@ -8,5 +8,5 @@ $document = &JFactory::getDocument();
 $document->addCustomTag('<script src="/modules/mod_maplink/js/maplink.js" async defer></script>');
 ?>
 <form action="<?=JRoute::_('index.php?option=com_voterapp');?>" method="post" id="josForm" name="josForm" class="form-validate">
-    <input type="text" id="address1" name="address1" value="<?=$address1;?>" class="inputbox" placeholder="<?=JText::_('STREET PLACEHOLDER');?>" />
+    <input type="text" id="address1" name="address1" value="<?=$address1;?>" class="inputbox" placeholder="<?=JText::_('ADDRESS PLACEHOLDER');?>" />
 </form>
