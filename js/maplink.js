4444b4ea9aa70d6c37c7c40d196b7dfdc92f8a10
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-07 09:19:14 -0500

    somewhat reducing js  towards functional target

--- js/maplink.js
+++ js/maplink.js
@@ -20,21 +20,22 @@ var AC = (function() {
 
   inner.fillInAddress = function() {
     // Get the place details from the inner.autoComplete object.
-    var place = inner.autoComplete.getPlace();
+//    var place = inner.autoComplete.getPlace();
     // Get each component of the address from the place details
     // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
+/*    for (var i = 0; i < place.address_components.length; i++) {
       var addressType = place.address_components[i].types[0];
       if (inner.returnData[addressType]) {
         var val = place.address_components[i][inner.returnData[addressType]];
         inner.formData[addressType] = val;
       }
-    }
-    document.getElementById('address1').value = inner.formData['street_number'] + ' ' + inner.formData['route'];
+    }*/
+    document.location='/index.php?option=com_voterapp&tmpl=component&address='+urlencode(document.getElementById('address1').value);
+    /*document.getElementById('address1').value = inner.formData['street_number'] + ' ' + inner.formData['route'];
     document.getElementById('city').value = inner.formData['locality'];
     document.getElementById('region').value = inner.formData['administrative_area_level_1'];
     document.getElementById('postcode').value = inner.formData['postal_code'];
-    document.getElementById('address2').focus();
+    document.getElementById('address2').focus();*/
   };
 
   inner.geolocate = function() {
