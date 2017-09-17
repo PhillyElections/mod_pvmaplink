84289ab1f7630e175c63399f1923ffb00b1d561d
matt murphy <mattyhead@gmail.com>
2017-09-17 07:03:08 -0400

    dropping optional map/replacing google

--- js/maplink.js
+++ js/maplink.js
@@ -1,56 +1,24 @@
-var AC = (function() {
-  'use strict';
-  var outer = {},
-    inner = {};
-
-  inner.geolocation = {
-    lat: 39.952464,
-    lng: -75.1662477
-  };
-
-  inner.fillInAddress = function() {
-    document.location='/index.php?option=com_voterapp&tmpl=component&address='+encodeURIComponent(document.getElementById('address').value);
-  };
-
-  inner.geolocate = function() {
-    // set center to city hall
-    var circle = new window.google.maps.Circle({
-      center: inner.geolocation,
-      radius: 15000
+window.addEvent("domready", function() {
+    var $=window.jQuery;
+    var apiURL = "https://apis.philadelphiavotes.com/autocomplete/";
+    var addressEl = $("#address");
+    
+    addressEl.autocomplete({
+        source: function(request, callback) {
+          var url=apiURL + encodeURIComponent(addressEl.val());
+            $.getJSON(url, function(response) {
+                if (response.status == "success") {
+                    var addresses = $.map(response.data, function(candidate) {
+                        return {
+                            label: candidate.label
+                        };
+                    });
+                    callback(addresses);
+                } else {
+                    callback([]);
+                }
+            });
+        },
+        select: function (event, ui) { addressEl.value=ui.item.value;window.location='index.php?option=com_voterapp&tmpl=component&address='+encodeURIComponent(ui.item.value)}
     });
-    inner.autoComplete.setBounds(circle.getBounds());
-  };
-
-  outer.complete = function() {
-  outer.map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(39.9500, -75.1642),
-    zoom: 12  });
-
-    if (typeof inner.autoComplete === 'undefined') {
-      inner.autoComplete = new google.maps.places.Autocomplete(
-        document.getElementById('address'), {
-          types: ['geocode']
-        });
-      inner.autoComplete.addListener('place_changed', function() {
-        inner.fillInAddress();
-      });
-    }
-    if (typeof inner.located || inner.located === false) {
-      inner.geolocate();
-      inner.located = true;
-    }
-  };
-
-  outer.init = function() {
-    var script = document.createElement('script');
-    script.id = '_gmaps';
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&callback=AC.complete&key=AIzaSyDG7jgg6RbsEKG7UFXsSPi7F5RyRDTasnE';
-    document.body.appendChild(script);
-  };
-
-  return outer;
-})();
-window.addEvent('domready', function() {
-  AC.init();
 });
--- mod_pvmaplink.php
+++ mod_pvmaplink.php
@@ -4,35 +4,14 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 
 $language = JFactory::getLanguage();
 $language->load('mod_pvmaplink', JPATH_ADMIN);
-$showMap = ($params->get('showmap') === 'TRUE');
 $document = &JFactory::getDocument();
-$document->addCustomTag('<script src="/modules/mod_pvmaplink/js/maplink.js" async defer></script>');
+$document->addStyleSheet("https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css");
+$document->addCustomTag('<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>');
+$document->addCustomTag('<script src="/modules/mod_pvmaplink/js/maplink.js"></script>');
+
 ?>
-<style>
-.pac-container, .pac-item {
-    width: inherit !important;
-}
-<?php
-if ($showMap) :
-?>
-#map-canvas {
-    margin-top: .25em;
-    margin-bottom: .25em;
-    width: 100%;
-    height: 100px;
-}
-<?php
-else:
-?>
-#map-canvas {
-display:none;
-}
-<?php
-endif;
-?>
-</style>
 <form action="/index.php" method="get" id="josForm" name="josForm" class="form-validate">
-    <input type="text" id="address" name="address" width="23em" value="" class="inputbox" placeholder="<?=JText::_('ADDRESS PLACEHOLDER');?>" />
+    <input type="text" id="address" name="address" width="90% !important" value="" class="inputbox" placeholder="<?=JText::_('ADDRESS PLACEHOLDER');?>" />
     <input type="hidden" name="option" value="com_voterapp" />
     <input type="hidden" name="tmpl" value="component" />
     <div id="map-canvas"></div>
