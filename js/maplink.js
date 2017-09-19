fbd8642f1407a6bd3dbfac6010df04ec803c80a6
Matthew Murphy <mattyhead@gmail.com>
2017-09-19 08:42:09 -0400

    Update maplink.js

--- js/maplink.js
+++ js/maplink.js
@@ -1,11 +1,8 @@
 window.addEvent("domready", function() {
-    var $=window.jQuery;
-    var apiURL = "https://apis.philadelphiavotes.com/autocomplete/";
-    var addressEl = $("#address");
-    
+    var $ = window.jQuery, apiURL = "https://apis.philadelphiavotes.com/autocomplete/", addressEl = $("#address");
     addressEl.autocomplete({
         source: function(request, callback) {
-          var url=apiURL + encodeURIComponent(addressEl.val());
+            var url = apiURL + encodeURIComponent(addressEl.val());
             $.getJSON(url, function(response) {
                 if (response.status == "success") {
                     var addresses = $.map(response.data, function(candidate) {
@@ -19,6 +16,9 @@ window.addEvent("domready", function() {
                 }
             });
         },
-        select: function (event, ui) { addressEl.value=ui.item.value;window.location='index.php?option=com_voterapp&tmpl=component&address='+encodeURIComponent(ui.item.value)}
+        select: function(event, ui) {
+            addressEl.value = ui.item.value;
+            window.location = "index.php?option=com_voterapp&tmpl=component&address=" + encodeURIComponent(ui.item.value);
+        }
     });
 });
